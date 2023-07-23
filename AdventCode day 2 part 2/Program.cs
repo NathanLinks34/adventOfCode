5d42d018f8a0f20d332233019a1dc35dd554cf2b
NathanLinks34 <139394188+NathanLinks34@users.noreply.github.com>
2023-07-23 19:42:52 +0200

    Add files via upload

---
+++ AdventCode day 2 part 2/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public enum Shape
+{
+    Rock,
+    Paper,
+    Scissors,
+}
+
+public struct Round
+{
+    public bool Win { get; }
+    public bool Draw { get; }
+    public Shape Shape { get; }
+
+    public Round(bool win, bool draw, Shape shape)
+    {
+        Win = win;
+        Draw = draw;
+        Shape = shape;
+    }
+
+    public int GetPoints()
+    {
+        int score = 0;
+
+        if (Win)
+        {
+            score += 6;
+        }
+
+        if (Draw)
+        {
+            score += 3;
+        }
+
+        switch (Shape)
+        {
+            case Shape.Rock:
+                score += 1;
+                break;
+            case Shape.Paper:
+                score += 2;
+                break;
+            case Shape.Scissors:
+                score += 3;
+                break;
+        }
+
+        return score;
+    }
+}
+
+public static class RPSHelper
+{
+    private static Shape FromChar(char s)
+    {
+        return s switch
+        {
+            'A' or 'X' => Shape.Rock,
+            'B' or 'Y' => Shape.Paper,
+            'C' or 'Z' => Shape.Scissors,
+            _ => throw new Exception("Invalid input"),
+        };
+    }
+
+    public static int GetScoreWithFirstStrategy(string input)
+    {
+        var matchRounds = input.Split(new[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+        return matchRounds.Select(round => GetScoreFirstStrat((FromChar(round[0]), FromChar(round[2])))).Sum();
+    }
+
+    public static int GetScoreWithSecondStrategy(string input)
+    {
+        var matchRounds = input.Split(new[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+        return matchRounds.Select(round => GetScoreSecondStrat((FromChar(round[0]), FromChar(round[2])))).Sum();
+    }
+
+    public static int GetScoreFirstStrat((Shape, Shape) input)
+    {
+        return input switch
+        {
+            (Shape.Scissors, Shape.Scissors) or
+            (Shape.Paper, Shape.Paper) or
+            (Shape.Rock, Shape.Rock) => new Round(false, true, input.Item2).GetPoints(),
+            (Shape.Scissors, Shape.Paper) or
+            (Shape.Rock, Shape.Scissors) or
+            (Shape.Paper, Shape.Rock) => new Round(false, false, input.Item2).GetPoints(),
+            (Shape.Paper, Shape.Scissors) or
+            (Shape.Rock, Shape.Paper) or
+            (Shape.Scissors, Shape.Rock) => new Round(true, false, input.Item2).GetPoints(),
+            _ => throw new Exception("Invalid input"),
+        };
+    }
+
+    public static int GetScoreSecondStrat((Shape, Shape) input)
+    {
+        return input switch
+        {
+            (Shape.Rock, Shape.Rock) => new Round(false, false, Shape.Scissors).GetPoints(),
+            (Shape.Paper, Shape.Rock) => new Round(false, false, Shape.Rock).GetPoints(),
+            (Shape.Scissors, Shape.Rock) => new Round(false, false, Shape.Paper).GetPoints(),
+            (Shape.Scissors, Shape.Scissors) => new Round(true, false, Shape.Rock).GetPoints(),
+            (Shape.Rock, Shape.Scissors) => new Round(true, false, Shape.Paper).GetPoints(),
+            (Shape.Paper, Shape.Scissors) => new Round(true, false, Shape.Scissors).GetPoints(),
+            (Shape.Rock, Shape.Paper) or
+            (Shape.Paper, Shape.Paper) or
+            (Shape.Scissors, Shape.Paper) => new Round(false, true, input.Item1).GetPoints(),
+            _ => throw new Exception("Invalid input"),
+        };
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        string input = System.IO.File.ReadAllText("inputday2.txt");
+
+        int totalScoreFirstStrat = RPSHelper.GetScoreWithFirstStrategy(input);
+        int totalScoreSecondStrat = RPSHelper.GetScoreWithSecondStrategy(input);
+
+        Console.WriteLine("Part 1: " + totalScoreFirstStrat);
+        Console.WriteLine("Part 2: " + totalScoreSecondStrat);
+    }
+}
---
+++ Advnt of code second day/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace RockPaperScissorsTournament
+{
+    class Program
+    {
+        static Dictionary<char, int> PLAY_D = new Dictionary<char, int>
+        {
+            { 'X', 1 }, { 'Y', 2 }, { 'Z', 3 }, { 'A', 1 }, { 'B', 2 }, { 'C', 3 }
+        };
+
+        static HashSet<Tuple<int, int>> WIN_D = new HashSet<Tuple<int, int>>
+        {
+            new Tuple<int, int>(1, 3), new Tuple<int, int>(2, 1), new Tuple<int, int>(3, 2)
+        };
+
+        static Dictionary<char, char> WIN_D_PT2 = new Dictionary<char, char>
+        {
+            { 'A', 'B' }, { 'B', 'C' }, { 'C', 'A' }
+        };
+
+        static Dictionary<char, char> LOSE_D_PT2 = new Dictionary<char, char>();
+        static Dictionary<char, Dictionary<char, char>> ACTION_D = new Dictionary<char, Dictionary<char, char>>();
+
+        static void Main(string[] args)
+        {
+            // Part 1
+            List<string> matchRounds = LoadStrategyGuide();
+            int totalScore = GetMyTotal(matchRounds);
+            Console.WriteLine("Part 1: " + totalScore);
+
+            // Part 2
+            InitializePart2Data();
+            List<string> alteredRounds = GetAlteredRounds(matchRounds);
+            int totalScorePt2 = GetMyTotal(alteredRounds);
+            Console.WriteLine("Part 2: " + totalScorePt2);
+        }
+
+        static List<string> LoadStrategyGuide()
+        {
+            List<string> result = new List<string>();
+            string filePath = "inputday2.txt";
+
+            using (StreamReader reader = new StreamReader(filePath))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    result.Add(line.Trim());
+                }
+            }
+
+            return result;
+        }
+
+        static int RoundScore(string matchRound)
+        {
+            char elf = matchRound[0];
+            char me = matchRound[matchRound.Length - 1];
+            int elfPlay = PLAY_D[elf];
+            int mePlay = PLAY_D[me];
+            return 6 * (WIN_D.Contains(new Tuple<int, int>(mePlay, elfPlay)) ? 1 : 0) + 3 * (mePlay == elfPlay ? 1 : 0) + mePlay;
+        }
+
+        static int GetMyTotal(List<string> matchRounds)
+        {
+            int totalScore = 0;
+            foreach (string match in matchRounds)
+            {
+                totalScore += RoundScore(match);
+            }
+            return totalScore;
+        }
+
+        static void InitializePart2Data()
+        {
+            foreach (var kvp in WIN_D_PT2)
+            {
+                LOSE_D_PT2.Add(kvp.Value, kvp.Key);
+            }
+
+            foreach (char c in "XYZ")
+            {
+                Dictionary<char, char> action = new Dictionary<char, char> { { c, c } };
+                ACTION_D.Add(c, action);
+            }
+            ACTION_D['X'].Add('A', LOSE_D_PT2['A']);
+            ACTION_D['X'].Add('B', LOSE_D_PT2['B']);
+            ACTION_D['X'].Add('C', LOSE_D_PT2['C']);
+            ACTION_D['Z'].Add('A', WIN_D_PT2['A']);
+            ACTION_D['Z'].Add('B', WIN_D_PT2['B']);
+            ACTION_D['Z'].Add('C', WIN_D_PT2['C']);
+        }
+
+        static string AlterRound(string matchRound)
+        {
+            char elf = matchRound[0];
+            char me = matchRound[matchRound.Length - 1];
+            return $"{elf} {ACTION_D[me][elf]}";
+        }
+
+        static List<string> GetAlteredRounds(List<string> matchRounds)
+        {
+            List<string> alteredRounds = new List<string>();
+            foreach (string match in matchRounds)
+            {
+                alteredRounds.Add(AlterRound(match));
+            }
+            return alteredRounds;
+        }
+    }
+}
